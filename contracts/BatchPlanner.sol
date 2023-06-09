525cf41fd0ea9a1036a3407e124205fad90d3d4b
icemanparachute <acfmichelsen@gmail.com>
2023-06-09 13:15:12 -0400

    update to new contracts only

--- contracts/BatchPlanner.sol
+++ contracts/BatchPlanner.sol
@@ -5,29 +5,6 @@ import './libraries/TransferHelper.sol';
 import './interfaces/IVestingTokenPlans.sol';
 import './interfaces/ILockedTokenPlans.sol';
 
-interface ILinearLock {
-  function createNFT(
-    address recipient,
-    address token,
-    uint256 amount,
-    uint256 start,
-    uint256 cliffDate,
-    uint256 rate
-  ) external;
-}
-
-interface ILinearVester {
-  function createNFT(
-    address recipient,
-    address token,
-    uint256 amount,
-    uint256 start,
-    uint256 cliffDate,
-    uint256 rate,
-    address vestingAdmin
-  ) external;
-}
-
 contract BatchPlanner {
   struct Plan {
     address recipient;
@@ -81,43 +58,6 @@ contract BatchPlanner {
         period,
         vestingAdmin,
         adminTransferOBO
-      );
-    }
-  }
-
-  function batchLinearLockup(address locker, address token, uint256 totalAmount, Plan[] memory plans) external {
-    TransferHelper.transferTokens(token, msg.sender, address(this), totalAmount);
-    SafeERC20.safeIncreaseAllowance(IERC20(token), locker, totalAmount);
-    for (uint16 i; i < plans.length; i++) {
-      ILinearLock(locker).createNFT(
-        plans[i].recipient,
-        token,
-        plans[i].amount,
-        plans[i].start,
-        plans[i].cliff,
-        plans[i].rate
-      );
-    }
-  }
-
-  function batchLinearVesting(
-    address locker,
-    address token,
-    uint256 totalAmount,
-    Plan[] memory plans,
-    address vestingAdmin
-  ) external {
-    TransferHelper.transferTokens(token, msg.sender, address(this), totalAmount);
-    SafeERC20.safeIncreaseAllowance(IERC20(token), locker, totalAmount);
-    for (uint16 i; i < plans.length; i++) {
-      ILinearVester(locker).createNFT(
-        plans[i].recipient,
-        token,
-        plans[i].amount,
-        plans[i].start,
-        plans[i].cliff,
-        plans[i].rate,
-        vestingAdmin
       );
     }
   }
