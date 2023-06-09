50f02497d487de8a667df9e84efe2d1f02b8adbb
icemanparachute <acfmichelsen@gmail.com>
2023-06-09 11:00:08 -0400

    make test token voting

--- contracts/test/Token.sol
+++ contracts/test/Token.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol';
 
-contract Token is ERC20 {
+contract Token is ERC20Votes {
   uint8 private _decimals;
 
   constructor(uint256 initialSupply, string memory name, string memory symbol) ERC20(name, symbol) {
