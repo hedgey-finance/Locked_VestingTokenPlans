6f7b9c501c0ea93ddce8c1ddb71a667abea7889b
icemanparachute <acfmichelsen@gmail.com>
2023-06-13 09:00:14 -0400

    add deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,22 @@
+const { ethers } = require("hardhat");
+
+async function deployNFTContract(artifact, args, uriBase) {
+    const Contract = await ethers.getContractFactory(artifact);
+    const contract = await Contract.deploy(...args);
+    await contract.deployed();
+    console.log(`new contract deployed to ${contract.address}`);
+    let uri = `${uriBase}${contract.address.toLocaleLowerCase()}/`;
+    const tx = await contract.updateBaseURI(uri);
+    console.log(`base URI tx hash: ${tx.hash}`);
+}
+
+async function deployPeriphery(args) {
+    const Planner = await ethers.getContractFactory('BatchPlanner');
+    const planner = await Planner.deploy();
+    await planner.deployed();
+    console.log(`new planner deployed to ${planner.address}`);
+    const Claimer = await ethers.getContractFactory('ClaimCampaign');
+    const claimer = await Claimer.deploy(...args);
+    await claimer.deployed();
+    console.log(`new claimer deployed to ${claimer.address}`);
+}
