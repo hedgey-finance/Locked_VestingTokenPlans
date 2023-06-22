d01bfd37640b8517e9c3d20d392a3b226cb52e77
icemanparachute <acfmichelsen@gmail.com>
2023-06-22 11:47:15 -0400

    sol coverage

--- .solcover.js
+++ .solcover.js
@@ -3,21 +3,8 @@ module.exports = {
     'test/Token.sol',
     'test/FakeVoteToken.sol',
     'test/NonVotingToken.sol',
+    'ERC721Delegate/ERC721Delegate.sol',
+    'ERC721Delegate/IERC721Delegate.sol',
   ],
   configureYulOptimizer: true,
 };
-
-/**
- * 'ERC721Delegate/ERC721Delegate.sol',
-    'ERC721Delegate/IERC721Delegate.sol',
-    'interfaces/IDelegateNFT.sol',
-    'interfaces/ILockupPlans.sol',
-    'interfaces/IVestingPlans.sol',
-    'libraries/TransferHelper.sol',
-    'LockupPlans/NonTransferable/TokenLockupPlans_Bound',
-    'LockupPlans/NonTransferable/VotingTokenLockupPlans_Bound',
-    'Lockups/TokenLockupPlans.sol',
-    'Lockups/VotingTokenLockupPlans.sol',
-    'Periphery/BatchPlanner.sol',
-    'Periphery/ClaimCampaigns.sol'
- */
